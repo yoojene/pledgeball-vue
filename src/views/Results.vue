e1d9d7f7b58adacdf4fdb290657f8f40aa839e49
yoojene <eugene.cross@gmail.com>
2019-08-31 22:22:32 +0100

    Testing Share

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -7,19 +7,35 @@
     </ion-title>
     </ion-toolbar>
     </ion-header>
-    <div>
+    <div class="ion-padding">
       <BarPledges>
       </BarPledges>
-    </div>
+      <ion-button expand="block" color="secondary" @click="share">
+        Share 
+    </ion-button>
+   </div>
   </ion-content>
 </template>
 <script>
 import BarPledges from '@/components/BarPledges.vue'
- 
+import { Plugins } from '@capacitor/core';
+
+const { Share } = Plugins;
+
 export default {
   name: 'Results',
   components: {
     BarPledges
+  },
+  methods: {
+    async share() {
+      const shareRet = await Share.share({
+        title: 'See cool stuff',
+        text: 'Really awesome thing you need to see right meow',
+        url: 'http://ionicframework.com/',
+        dialogTitle: 'Share with buddies'
+      });
+    }
   }
 
 }
