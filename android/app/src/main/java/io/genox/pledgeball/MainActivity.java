a831316605abe7d96c7b7716091ce05efb6dc738
yoojene <eugene.cross@gmail.com>
2019-10-13 22:18:06 +0100

    Back out capacitor-firebase-auth setup platform changes

--- android/app/src/main/java/io/genox/pledgeball/MainActivity.java
+++ android/app/src/main/java/io/genox/pledgeball/MainActivity.java
@@ -6,8 +6,6 @@ import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
 
 import java.util.ArrayList;
-import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
-
 
 public class MainActivity extends BridgeActivity {
   @Override
@@ -18,9 +16,6 @@ public class MainActivity extends BridgeActivity {
     this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
       // Additional plugins you've installed go here
       // Ex: add(TotallyAwesomePlugin.class);
-      add(CapacitorFirebaseAuth.class);
     }});
-
-
   }
 }
